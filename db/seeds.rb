ea4370c9ed0ebadf2d8ff7f04d70b2c518f49881
botiuk <svetabotiuk@gmail.com>
2024-04-09 13:50:26 +0300

    add seeds for Department and worker_photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,24 @@ user.update!(
     )
 end
 
+ActiveStorage::Blob.create!(
+    key: 'pbcj80wo1ggewlfs6zfrjqhg93va',
+    filename: 'doctor_empty_photo.jpg',
+    content_type: 'image/jpeg',
+    metadata: '{"identified":true,"width":380,"height":380,"analyzed":true}',
+    service_name: 'cloudinary',
+    byte_size: 5704,
+    checksum: 'Yp8xTVxnrsK16TZ6wJaPbw=='
+)
+(1..100).each.each do |worker_id|
+    ActiveStorage::Attachment.create!(
+        record_type: 'Worker',
+        record_id: worker_id,
+        name: 'worker_photo',
+        blob_id: 1
+    )
+end
+
 25.times do
     Position.create(
         name: Faker::Job.unique.position,
@@ -40,5 +58,12 @@ end
         worker_id: worker_id,
         position_id: rand(1..25),
         start_date: Faker::Date.between(from: 1.month.ago, to: Date.today)
+    )
+end
+
+15.times do
+    Department.create(
+        abbreviation: Faker::Alphanumeric.unique.alphanumeric(number: 4, min_alpha: 4),
+        name: Faker::Company.unique.department
     )
 end
