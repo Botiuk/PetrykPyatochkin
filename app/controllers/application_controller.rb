2d61ca00cd46be29712278ab88872392a8de85f5
botiuk <svetabotiuk@gmail.com>
2024-04-10 11:45:34 +0300

    update resources in routes, add page_not_found to aplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     include Pagy::Backend
     
     before_action :authenticate_user!
+
+    def page_not_found
+        render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :workers, :positions, :worker_positions, :departments
+  resources :workers, except: :destroy
+  resources :positions, except: :destroy
+  resources :worker_positions, except: [:index, :show, :destroy]
+  resources :departments
 
   # Defines the root path route ("/")
   root "workers#index"
+
+  match '*unmatched', to: 'application#page_not_found', via: :all
 end
